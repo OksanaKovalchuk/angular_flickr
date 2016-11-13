f61f7368c98368827503503d47d39391aee39aea
OksanaKovalchuk <kovalchuk.oksana17@gmail.com>
2016-11-13 05:29:58 +0200

    showing sizes and routing

--- app/js/photo/photo.js
+++ app/js/photo/photo.js
@@ -3,12 +3,13 @@
 angular.module('photo', ['ngRoute'])
 
     .config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/photo-with-size', {
+        $routeProvider.when('/photo/:id/size/:heightx:width/:label', {
             templateUrl: 'js/photo/photo.html',
             controller: photoCtrl
         });
     }]);
 
-function photoCtrl($scope) {
+function photoCtrl($scope, $routeParams) {
     console.log(this);
+    console.log($routeParams);
 }
--- app/js/photoSizes/photoSizes.js
+++ app/js/photoSizes/photoSizes.js
@@ -3,13 +3,43 @@
 angular.module('photoSize', ['ngRoute'])
 
     .config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/photo:id', {
+        $routeProvider.when('/photo/:id', {
             templateUrl: 'js/photoSizes/photoSizes.html',
             controller: PhotoSizeController
         });
     }]);
 
-function PhotoSizeController($scope) {
-    console.log(this);
-    $scope.id = 1;
+function PhotoSizeController($scope, $http, $location, $routeParams) {
+
+    console.log($location);
+    $scope.id = $routeParams.id;
+    $scope.getSizeOfImage = function(id){
+        $http({
+            method:"GET",
+            url: "https://api.flickr.com/services/rest",
+            params:{
+                method:"flickr.photos.getSizes",
+                api_key:"cd51c35deb0b194c8c3ccbf6e18954c5",
+                photo_id: id,
+                text: $scope.searchText,
+                format: "json",
+                nojsoncallback:1
+            }
+        }).success(function (data) {
+            $scope.sizes = data.sizes;
+            $scope.sourceImage = $scope.sizes.size[1].source;
+            console.log(data);
+        })
+    };
+    $scope.getSizeOfImage($scope.id);
+    $scope.send = function(url){
+      // $routeParams.source = url;
+        console.log($routeParams);
+    };
+    $scope.getImageBySize = function(size, photo){
+        console.log(size);
+        console.log(photo);
+        $location.path("/photo/"+$scope.id+"/size/"+size.height+"x"+size.width+"/"+size.label[0]);
+
+    };
 }
